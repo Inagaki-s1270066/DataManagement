39c1514b8f5e021c3520b2bceeae9905de74c15d
稲垣匡隆 <r-aim614k@wlan-napt-001.u-aizu.ac.jp>
2021-07-02 14:23:54 +0900

    Won/lost message added

--- dice.php
+++ dice.php
@@ -10,5 +10,9 @@ $sum = $D1 + $D2;
 
 echo "Die 1 : $D1 \n";
 echo "Die 2 : $D2 \n";
-echo "Total value : $sum \n"
+echo "Total value : $sum \n";
+
+if($sum > 7) echo "You won \n";
+else echo "You lost \n";
+
 ?>
